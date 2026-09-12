89d9c13cf7f60ae2c052b6fb1051c76f484b727b
NosferatuAlasi <148828746+NosferatuAlasi@users.noreply.github.com>
2026-09-11 23:15:22 -0600

    feat(parcial01) - add db initializer

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -16,6 +16,20 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) { }
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await DbInitializer.SeedDataAsync(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred while migrating or seeding the database.");
+}
+
 app.MapControllers();
 
 app.Run();
